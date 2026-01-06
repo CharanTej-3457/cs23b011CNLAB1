9835194a61c5537f02c869fd3c7e70cbfccd74bf
CharanTej-3457 <cs23b011@iittp.ac.in>
2026-01-06 16:03:34 +0530

    Update cs23b011_nrz_am.py
    adedd plots for all the signals(sub plots)

--- CS23B011/cs23b011_nrz_am.py
+++ CS23B011/cs23b011_nrz_am.py
@@ -1,9 +1,12 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 data = [1,0,1,0,1,1,0]
-fc = 20
+fc = 20 # Carrier frequency in Hz
 bits_per_sample = 200
+fs = 200 # Sampling frequency in Hz
+
 nrz_sig = []
 
 for bit in data:
@@ -14,14 +17,44 @@ for bit in data:
         nrz_sig.extend([-1] * bits_per_sample)
 
 nrz_sig = np.array(nrz_sig)
-#commit-1
-#print(nrz_sig , nrz_sig.dtype)
+
 
 t = np.arange(len(nrz_sig)) / fs
-carrier_signal = np.sin(2 * np.pi * fc * t)
-#commit-2
-am_signal = (1+nrz_sig) * carrier_signal
-#commit - 3
 
+
+carrier_signal = 5*np.sin(2 * np.pi * fc * t)
+
+
+am_signal = (4+nrz_sig) * carrier_signal
+
+
+plt.figure(figsize=(12, 8))
+
+# Subplot 1: NRZ Message Signal
+plt.subplot(3, 1, 1)
+plt.plot(t, nrz_sig, drawstyle='steps-pre')
+plt.title("NRZ Message Signal")
+plt.ylabel('Amplitude')
+plt.grid(True)
+plt.yticks([-1, 1])
+
+# Subplot 2: Carrier Signal
+plt.subplot(3, 1, 2)
+plt.plot(t, carrier_signal)
+plt.title("Carrier Signal (20 Hz)")
+plt.ylabel('Amplitude')
+plt.grid(True)
+
+# Subplot 3: AM Modulated Signal
+plt.subplot(3, 1, 3)
+plt.plot(t, am_signal)
+plt.title("AM Modulated Signal")
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
+
+plt.tight_layout()
+plt.savefig("amplots.pdf")
+plt.show()
 
 
