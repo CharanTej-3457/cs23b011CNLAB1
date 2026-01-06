e22767d93b1cf543147ea785906aba00bb36f7f7
CharanTej-3457 <cs23b011@iittp.ac.in>
2026-01-06 15:39:20 +0530

    Update cs23b011_nrz_am.py
    re verified the initialization of nrz encoding of given data sequence

--- CS23B011/cs23b011_nrz_am.py
+++ CS23B011/cs23b011_nrz_am.py
@@ -1,10 +1,19 @@
 import numpy as np
-from matplotlib.pyplot import pyplot as plt
+import matplotlib.pyplot as plt
 
 data = [1,0,1,0,1,1,0]
-data_seq = np.array(data)
-samples_per_bit = 200
 fc = 20
-data_seq_bits = np.zeros(len(data_seq) * samples_per_bit)
-print(data_seq_bits , data_seq_bits.dtype)
+bits_per_sample = 200
+nrz_sig = []
 
+for bit in data:
+
+    if bit == 1:
+        nrz_sig.extend([1] * bits_per_sample)
+    else:
+        nrz_sig.extend([-1] * bits_per_sample)
+
+nrz_sig = np.array(nrz_sig)
+
+
+#print(nrz_sig , nrz_sig.dtype)
