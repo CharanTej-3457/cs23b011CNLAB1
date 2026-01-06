8d60de6905a4007cacffd9cd2429951409a59887
CharanTej-3457 <cs23b011@iittp.ac.in>
2026-01-06 15:17:48 +0530

    Update cs23b011_nrz_am.py
    initialised NRZ sequence for the given data sequence

--- CS23B011/cs23b011_nrz_am.py
+++ CS23B011/cs23b011_nrz_am.py
@@ -1,2 +1,10 @@
-import pandas as pd
+import numpy as np
+from matplotlib.pyplot import pyplot as plt
+
+data = [1,0,1,0,1,1,0]
+data_seq = np.array(data)
+samples_per_bit = 200
+fc = 20
+data_seq_bits = np.zeros(len(data_seq) * samples_per_bit)
+print(data_seq_bits , data_seq_bits.dtype)
 
